aa184cbb4045e514fdefd9b17751402691ec2bd4
dahyeshdesale <dreamdesale108@gmail.com>
2024-12-13 15:43:39 +0530

    new start v2

--- Main.tf
+++ Main.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "name" {
-  
-}
-
 resource "aws_instance" "public1" { 
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro" 
